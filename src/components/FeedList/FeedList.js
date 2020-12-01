d7a053d3681b2e87450352bf91c3e59e6bfe71e2
Dawid Szpener <dawidszpener141@gmail.com>
2020-12-01 21:48:37 +0000

    Implement infinite scroll functionality to <FeedList />

--- src/components/FeedList/FeedList.js
+++ src/components/FeedList/FeedList.js
@@ -1,48 +1,75 @@
 import React, { Component } from 'react'
 import axios from 'axios'
+import InfiniteScroll from 'react-infinite-scroll-component';
 
+import './FeedList.css'
 import Post from '../Post/Post'
 import Aux from '../Aux/Aux'
 
 class FeedList extends Component {
   state = {
-    list: []
+    list: [],
+    shownPosts: [],
+    counter: 0
   }
 
   componentDidMount() {
-    axios.get('https://pastebin.pl/view/raw/e1658aa0')
+    axios.get(`https://pastebin.pl/view/raw/e1658aa0`)
     .then(res => {
       let postList = []
-      Object.values(res.data.posts).map(post => {
+      Object.values(res.data.posts).map((post) => {
         return( 
-          postList.push(
-            <Post
-              title={post.title}
-              thumb={post.thumb}
-              data={post.data}
-              excerpt={post.excerpt}
-              url={post.url}
-            />
-          )
+          postList.push(post)
         )
       })
       this.setState({list: postList})
     })
+    .then(() => {
+      let posts = this.state.list.slice(0-10).map(
+        post => {
+          return(post)
+        }
+      )
+      let counter = this.state.counter + 10
+      this.setState({shownPosts: posts, counter: counter})
+    })  
   }
+
+  fetchMoreData = () => {
+    console.log('FetchMoreData')
+    setTimeout(() => {
+      this.setState({
+        shownPosts: [...this.state.shownPosts, ...this.state.list.slice(this.state.counter, this.state.counter + 10)],
+        counter: this.state.counter + 10
+      });
+    }, 1500);
+  };
 
   render() {
     return(
-      <div>
+      <div id="Container">
         <h1>List</h1>
-        <ul className="FeedList">
-          {this.state.list.map(post => {
+        <InfiniteScroll
+          dataLength={this.state.shownPosts.length}
+          next={this.fetchMoreData}
+          hasMore={true}
+          loader={<h4>Loading...</h4>}
+        >
+          {this.state.shownPosts.map((post, index) => {
             return(
               <Aux>
-                {post}
+                <Post
+                  key={index}
+                  title={post.title}
+                  thumb={post.thumb}
+                  data={post.data}
+                  excerpt={post.excerpt}
+                  url={post.url}
+                />
               </Aux>
             )
           })}
-        </ul>
+        </InfiniteScroll>
       </div>
     )
   }
--- src/components/Post/Post.js
+++ src/components/Post/Post.js
@@ -2,12 +2,11 @@ import React from 'react'
 import  './Post.css'
 
 const post = (props) => (
-  <li className='Post'>
-    <img src={props.thumb} alt='pic'/>
+  <li className='Post' >
+    <img src={props.thumb} alt='pic' onClick={() => props.url}/>
     <h5>{props.title}</h5>
     <h6>{props.data}</h6>
     {props.excerpt}
-    {props.url}
   </li>
 )
 
